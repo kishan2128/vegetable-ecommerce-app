c63022eee47a79d02f6eb7ac9748e45d47d0e572
kishan2128 <kushwahakishan2128@gmail.com>
2025-03-29 05:38:01 +0530

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 Sabji Ecommerce. All Rights Reserved.</p>
+      <p>📧 Contact: support@sabjiapp.com | 📞 Phone: +91-7878318236</p>
+    </footer>
+  );
+};
+
+export default Footer;
