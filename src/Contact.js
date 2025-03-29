87d3fd46b95f5c6bdc10b35e4d8e2809b720ec92
kishan2128 <kushwahakishan2128@gmail.com>
2025-03-29 05:39:25 +0530

    Create Contact.js

---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <h2>Contact Us</h2>
+      <p>📍 Address: Sabji Ecommerce, Main Market, India</p>
+      <p>📧 Email: support@sabjiapp.com</p>
+      <p>📞 Phone: +91-7878318236</p>
+    </div>
+  );
+};
+
+export default Contact;
